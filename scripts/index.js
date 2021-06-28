69fcee47e2038c03544e782f3e7af5e817d7f843
Richard Melkonian <r.v.melkonian@gmail.com>
2021-06-28 20:46:57 +0300

    commit

---
+++ scripts/index.js
@@ -0,0 +1,19 @@
+const hre = require("hardhat");
+const ethers = require('ethers');
+const USDT_ABI = require('../abis/USDT_ABI.json')
+
+async function main() {
+
+const usdt = "0xdAC17F958D2ee523a2206206994597C13D831ec7";
+const provider = new ethers.providers.WebSocketProvider("wss://eth-mainnet.ws.alchemyapi.io/v2/gRR0KK-rRxTfSUdGd_g11RvpjrgCRN8a");
+const contract = new ethers.Contract(usdt, USDT_ABI, provider)
+
+contract.on('Transfer', (from, to, value) => console.log(from, to, value))
+ }
+
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
